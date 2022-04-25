84dc15326300549d93ff0ff794c65fade875fe44
ningk321 <62751724+ningk321@users.noreply.github.com>
2022-04-26 01:49:50 +0800

    Update third_party.sh

--- third_party.sh
+++ third_party.sh
@@ -9,6 +9,7 @@
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 sed -i '$a src-git jerrykuku https://github.com/jerrykuku/openwrt-package.git' feeds.conf.default
 sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
 
 # Use luci 18.06 on lienol feed
 #
